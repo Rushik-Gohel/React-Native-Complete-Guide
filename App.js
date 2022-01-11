441240c31fd7dc05f139cfe29981f91e310db1c7
rushik <rushikgohel@gmail.com>
2022-01-11 12:51:17 +0530

    Flat List introduced

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  FlatList,
+} from "react-native";
 
 export default function App() {
-
-  const [enteredGoal, setEnteredGoal] = useState('');
+  const [enteredGoal, setEnteredGoal] = useState("");
   const [courseGoals, setCourseGoals] = useState([]);
 
   const goalInputHandler = (enteredText) => {
@@ -12,23 +19,32 @@ export default function App() {
   };
 
   const addGoalHandler = () => {
-    setCourseGoals(currentGoals => [...currentGoals, enteredGoal])
-  }
+    setCourseGoals((currentGoals) => [
+      ...currentGoals,
+      { id: Math.random().toString(), value: enteredGoal },
+    ]);
+  };
 
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput 
-          placeholder="Course Goal" 
+        <TextInput
+          placeholder="Course Goal"
           style={styles.input}
           onChangeText={goalInputHandler}
-          value = {enteredGoal} 
+          value={enteredGoal}
         />
-        <Button title="Add" onPress={addGoalHandler}/>
+        <Button title="Add" onPress={addGoalHandler} />
       </View>
-      <View>
-        {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
-      </View>
+      <FlatList
+      keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={(itemData) => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value }</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
@@ -47,5 +63,12 @@ const styles = StyleSheet.create({
     borderColor: "black",
     borderWidth: 1,
     padding: 10,
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1,
   },
 });
